e14d1f67460eaa162bf094f34eb1fa1c139533c3
vzloman546-design <vzloman546@gmail.com>
2026-09-25 15:16:03 +0300

    Refresh offline cache for grid layout

--- turnstile-inspection/sw.js
+++ turnstile-inspection/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'turnstile-inspection-v6';
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg","./assets-init.js","./asset-page1-1.js","./asset-page2-1.js","./asset-page2-2.js","./asset-page3-1.js","./asset-page3-2.js","./asset-page4-1.js","./asset-page4-2.js","./asset-page5-1.js","./asset-page5-2.js","./asset-page6-1.js","./asset-atlas-1.js","./asset-atlas-2.js","./asset-atlas-3.js","./assets-meta.js","./assets-final.js"];
+const CACHE = 'turnstile-inspection-v7';
+const ASSETS = ["./","./index.html","./styles.css?v=7","./app.js?v=7","./manifest.webmanifest","./icon.svg","./assets-init.js","./asset-page1-1.js","./asset-page2-1.js","./asset-page2-2.js","./asset-page3-1.js","./asset-page3-2.js","./asset-page4-1.js","./asset-page4-2.js","./asset-page5-1.js","./asset-page5-2.js","./asset-page6-1.js","./asset-atlas-1.js","./asset-atlas-2.js","./asset-atlas-3.js","./assets-meta.js","./assets-final.js"];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
