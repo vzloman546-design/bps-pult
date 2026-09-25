a51fb298ee76df6ad3a29458086bec9dba103e7d
vzloman546-design <vzloman546@gmail.com>
2026-09-25 13:55:49 +0300

    Publish turnstile inspection PWA

---
+++ turnstile-inspection/assets-final.js
@@ -0,0 +1,1 @@
+window.TURNSTILE_ASSETS={pages:window.__TURNSTILE_ASSET_PARTS.pages,atlas:window.__TURNSTILE_ASSET_PARTS.atlas,meta:window.__TURNSTILE_ASSET_PARTS.meta};delete window.__TURNSTILE_ASSET_PARTS;
